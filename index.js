a9fe68d17ccb5e91c77357b93397d57fe91c8d2f
arnav kumar gupta <akarnavgupta321@gmail.com>
2024-04-05 16:26:21 +0530

    Programming patsala Day4

---
+++ index.js
@@ -0,0 +1,33 @@
+// let a= new String("hello")
+// let b="hello"
+// const obj ={
+//     name:'arnav',
+//     lastname :'gupta',
+// }
+// const obj2 ={
+//     'name':'arnav',
+//     'lastname' :'gupta',
+// }
+// console.log(obj);
+// document.write(obj);
+
+// const srt =JSON.stringify(obj);
+// document.write(str)
+
+// const obj={
+//     "name":"arnav",
+//     "lastName" :"gupta"
+// }
+// // obj.name=20;
+
+// console.log(obj);
+
+
+// console.log("hello");
+// const arr =[1,2,34];
+// arr=[2,3,4];
+// console.log(arr);
+
+const arr1=[1,2,34];
+arr[10]=20;
+console.log(arr);
